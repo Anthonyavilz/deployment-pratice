5030dd03f88eda2c1f788d46928cee3b13e6bb8b
Phillip Avila <Anthonyavilz@gmail.com>
2023-05-31 16:32:06 -0500

    updated buttons

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
-const btn = document.querySelector('button')
+const btn = document.querySelector('.first-btn')
+const secondBtn = document.querySelector('.second-btn')
 
 const clickHandler = () => alert('Hello')
 
 btn.addEventListener('click', clickHandler)
+secondBtn.addEventListener('click', clickHandler)
